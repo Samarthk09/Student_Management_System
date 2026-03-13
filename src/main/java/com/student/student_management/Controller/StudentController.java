c4c156e3377193a9700689da6b0836db949a1da7
Samarth Kolhe <samarthkolhe09@gmail.com>
2026-03-13 13:15:44 +0530

    Create StudentController.java

---
+++ src/main/java/com/student/student_management/Controller/StudentController.java
@@ -0,0 +1,10 @@
+package com.student.student_management.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+}
